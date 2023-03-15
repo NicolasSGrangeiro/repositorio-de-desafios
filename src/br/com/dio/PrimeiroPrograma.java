f5d62dbc9ce8a214507b7bc2e348610304cfbcde
NicolasSGrangeiro <nicolas-100-sousa@hotmail.com>
2023-03-15 14:24:05 -0300

    Inserindo Classs Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,11 +5,48 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato ();
-
         System.out.println(gato);
+
+        Livro livro = new Livro("Alice", 300);
+        System.out.println(livro);
 
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World " + (a + b));*/
+    }
+
+}
+
+class Livro {
+    private String nome;
+    private  Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+    
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
     }
 }
